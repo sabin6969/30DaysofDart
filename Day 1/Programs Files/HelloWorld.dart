639fd4019bf27fd09a8f107eff8039600ef360c4
Sabin Poudel <111721663+sabin6969@users.noreply.github.com>
2023-06-08 13:22:34 +0530

    Update HelloWorld.dart

--- Day 1/Programs Files/HelloWorld.dart
+++ Day 1/Programs Files/HelloWorld.dart
@@ -1,1 +1,11 @@
+// void is a return type and main is a special function from where execution of our program begins
+import 'dart:io';
 
+void main() {
+  // print is an inbuilt function that is used to display anything on a output screen or terminal
+  print("Hello World");
+
+  // we even use .write method
+  // this method is derived from dart:io module
+  stdout.write("Hello World");
+}
