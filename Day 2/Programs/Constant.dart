fe5088890dfba26a5105f2798a0e9e1bf1d4a7a8
Sabin Poudel <111721663+sabin6969@users.noreply.github.com>
2023-06-09 21:07:19 +0530

    Update Constant.dart

--- Day 2/Programs/Constant.dart
+++ Day 2/Programs/Constant.dart
@@ -1,1 +1,6 @@
-
+void main() {
+  const double pi = 3.14159;
+  // Constant can be declared using const keyword
+  // The value of constant remains unchanged throughout the program
+  print(pi);
+}
