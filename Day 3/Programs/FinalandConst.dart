c95fa42f2e9f0594203b549e9f49b6f1034b9488
Sabin Poudel <111721663+sabin6969@users.noreply.github.com>
2023-06-10 20:09:06 +0530

    Update FinalandConst.dart

--- Day 3/Programs/FinalandConst.dart
+++ Day 3/Programs/FinalandConst.dart
@@ -1,1 +1,8 @@
-
+void main() {
+  //const is static constant variable we have to declare its data type
+  const double pi = 3.14;
+  print(pi);
+  // final is dynamic constant variable that means data type shouldnot be mentioned
+  final a = 120;
+  print(a);
+}
