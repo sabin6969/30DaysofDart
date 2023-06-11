4bfc082df9763de3bfc60472707455be7c1f29ed
Sabin Poudel <111721663+sabin6969@users.noreply.github.com>
2023-06-11 18:58:45 +0530

    Update forloop.dart

--- Day 4/Programs/forloop.dart
+++ Day 4/Programs/forloop.dart
@@ -1,1 +1,10 @@
+import 'dart:io';
 
+void main() {
+  stdout.write("Enter a number: ");
+  dynamic number = stdin.readLineSync();
+  int no = int.parse(number);
+  for (int i = 0; i < no; i += 2) {
+    stdout.write(i);
+  }
+}
